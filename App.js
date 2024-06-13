a97f8ae9b31b82e445dd9be4136df23004fda256
Daniel Rovira <danielroviratorres@gmail.com>
2024-06-13 20:40:03 -0300

    google auth

--- App.js
+++ App.js
@@ -3,14 +3,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const passport = require('passport');
+const session = require('express-session');
+// var csrf = require('csurf');
+var logger = require('morgan');
+var SQLiteStore = require('connect-sqlite3')(session);
 const app = express();
+
+const authRouter = require('./routes/auth');
 const userRoutes = require("./routes/user-routes");
 const DB = require("./routes/financial-control-routes");
 
+app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.set("trust proxy", true)
 app.use(cors({ credentials: true, origin: process.env.CORS }))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    store: new SQLiteStore({ db: 'sessions.db', dir: 'var/db' })
+  }));
+// app.use(csrf());
+app.use(passport.authenticate('session'));
+
+app.use('/api/oauth', authRouter);
 app.use("/api", userRoutes);
 app.use(`/api/${process.env.DB}`, DB);
 mongoose.set("strictQuery", false);     // Will be the default after Mongoose 7. Remove after that

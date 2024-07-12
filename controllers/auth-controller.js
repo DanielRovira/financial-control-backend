0430ae4f603356df04cfe0c1ce1eee44881fb312
Daniel Rovira <danielroviratorres@gmail.com>
2024-07-12 19:19:50 -0300

    Revert ""
    This reverts commit 5c23e134330f3130d51be2f4cc2ad479f6850091.

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -59,10 +59,10 @@ const oauthLogin = async (req, res, next) => {
         );
         
         // return res.redirect(`${process.env.CORS}/api/getUser`)
-        // return res.redirect(`${process.env.CORS}`)
-        return res
-        .status(200)
-        .json({ message: "Successfully Logged In", user: {name: user.name, email: user.email}, token, status: 200 });
+        return res.redirect(`${process.env.CORS}`)
+        // return res
+        // .status(200)
+        // .json({ message: "Successfully Logged In", user: {name: user.name, email: user.email}, token, status: 200 });
 };
 
 exports.oauthLogin = oauthLogin;
